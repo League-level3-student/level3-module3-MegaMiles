5a85c2add09632848f2e32d891480d7cdf926230
MegaMiles <55776997+MegaMiles@users.noreply.github.com>
2022-10-17 17:07:06 -0700

    Level 3 commit

--- src/_00_Intro_To_String_Methods/_01_StringMethods.java
+++ src/_00_Intro_To_String_Methods/_01_StringMethods.java
@@ -34,13 +34,21 @@ public class _01_StringMethods {
 
     // Given Strings s1 and s2, return the longer String
     public static String longerString(String s1, String s2) {
-        return null;
+    if (s1.length() > s2.length()) {
+	return s1;	
+	}
+    else {
+    return s2;
+    }
     }
 
     // If String s contains the word "underscores", change all of the spaces
     // to underscores
     public static String formatSpaces(String s) {
-        return null;
+    if(s.contains("underscores")) {
+        return s.replaceAll(" ", "_");
+    }
+	return s;
     }
 
     // Return the name of the person whose LAST name would appear first if they
@@ -48,12 +56,38 @@ public class _01_StringMethods {
     // You cannot assume there are no extra spaces around the name, but you can
     // assume there is only one space between the first and last name
     public static String lineLeader(String s1, String s2, String s3) {
-        return null;
+    s1 = s1.trim();
+    s2 = s2.trim();
+    s3 = s3.trim();	
+    String[] names1 = s1.split(" ");
+    String[] names2 = s2.split(" ");
+    String[] names3 = s3.split(" ");
+    if(names1[1].compareTo(names2[1]) < 0) {
+    	if(names1[1].compareTo(names3[1])  < 0) {
+    		return s1;
+    	}
+    }
+    if(names2[1].compareTo(names3[1]) < 0) {
+    	if(names2[1].compareTo(names1[1])  < 0) {
+    		return s2;
+    	}
+    }
+    if(names3[1].compareTo(names1[1]) < 0) {
+    	if(names3[1].compareTo(names2[1])  < 0) {
+    		return s3;
+    	}
+    }
+	return null;
     }
 
     // Return the sum of all numerical digits in the String
     public static int numeralSum(String s) {
-        return 0;
+    for (int i = 0; i < s.length(); i++) {
+		if (s.) {
+			
+		}
+	}
+    return (Integer) null;
     }
 
     // Return the number of times String substring appears in String s
